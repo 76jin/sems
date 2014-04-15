8d7cedf5007ca14817072b8b7eb3db5389550016
ranian129 <ranian129@gmail.com>
2014-04-15 17:33:17 +0900

    .

---
+++ sems2/src/Ex0414/SubjectDao.java
@@ -0,0 +1,24 @@
+package basic.exam06.jdbc.up7;
+
+import java.util.List;
+
+// SubjectDao를 인터페이스로 만든다.
+// 각 DBMS 별로 구현체를 만든다.
+// Dao의 교체가 쉬워진다.
+
+public interface SubjectDao {
+	void insert(SubjectVo subject) throws Throwable;
+	List<SubjectVo> list(int pageNo, int pageSize) throws Throwable;
+	SubjectVo detail(int no) throws Throwable;
+	void update(SubjectVo subject) throws Throwable;
+	void delete(int no) throws Throwable;
+}
+
+
+
+
+
+
+
+
+
